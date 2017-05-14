04146693f658d8d61696973979cc2fb1c01b9fd4
wsavd <tuve@rootfest.net>
2017-05-14 18:49:47 +0300

    как из JSON в JS и наоборот

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,33 @@
+/*
+Если нужно с сервера взять объект с данными и передать на клиенте,
+то в качестве промежуточного формата – для передачи по сети,
+почти всегда используют именно его.
+*/
+/*
+//javascript object to JSON object
+var cardJS = {
+    title : 'Карточка',
+    body : 'Текст карточки'
+}
+console.log(cardJS);//вывести JS объект
+var cardJSON = JSON.stringify(cardJS);//преобразуем в JSON объект
+console.log(cardJSON)//json объект
+*/
+
+//JSON object to JS object
+//строго в двойных скобках
+var cardJSON = '{ "title" : "Другая карточка", "body": "Текст другой карточки"}'
+console.log(cardJSON)//вывести JSON объект
+cardJS = JSON.parse(cardJSON);//преобразование в javascript объект
+console.log(cardJS)//JS объект
+
+
+
+
+
+
+
+
 $(function() {
      $( ".column" ).sortable({
          connectWith: ".column", //взаимное перемещение межу
